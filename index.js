7e32cd966461f1431b5a1410e39cebed611cc747
Jason To <jasto@ucdavis.edu>
2018-09-05 17:22:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Add your functions and code here
 function destructivelyAppendKitten(name){
-  var array = [];
-  array.push(name);
-  return array;
+  var kitten = [];
+  kitten.push(name);
+  return kitten;
 }
