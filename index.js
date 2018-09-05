e96a4cc08af5ffd7dac795da82aab94f3ce6fec9
Jason To <jasto@ucdavis.edu>
2018-09-05 17:57:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,25 +8,21 @@ function destructivelyAppendKitten(name){
 }
 
 function destructivelyPrependKitten(name){
-  var kitten = [];
   kitten.push(name);
   return kitten;
 }
 
 function destructivelyRemoveLastKitten(){
-  var kitten = [];
   kitten.pop();
   return kitten;
 }
 
 function destructivelyRemoveFirstKitten(){
-  var kitten = [];
   kitten.unshift();
   return kitten;
 }
 
 function appendKitten(name){
-  var kitten = [];
   return [...kitten, name];
 }
 
@@ -36,12 +32,10 @@ function prependKitten(name){
 }
 
 function removeLastKitten(){
-  var kitten = [];
   kittens = kitten.slice(0, array.length - 1);
   return kittens;
 }
 
 function removeFirstKitten(){
-  var kitten = [];
   return kitten.slice(1);
 }
